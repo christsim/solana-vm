a9254a8a9100589d66f0f784029e9e3241a7afe8
christsim <28301107+christsim@users.noreply.github.com>
2025-02-11 11:01:49 +0200

    made add users use an external file, added more performance tuning

--- solana/3-performance-tuning.sh
+++ solana/3-performance-tuning.sh
@@ -7,6 +7,21 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+sudo bash -c "cat >/etc/sysctl.d/21-agave-validator.conf <<EOF
+# Increase UDP buffer sizes
+net.core.rmem_default = 134217728
+net.core.rmem_max = 134217728
+net.core.wmem_default = 134217728
+net.core.wmem_max = 134217728
+
+# Increase memory mapped files limit
+vm.max_map_count = 1000000
+
+# Increase number of allowed open file descriptors
+fs.nr_open = 1000000
+EOF"
+sudo sysctl -p /etc/sysctl.d/21-agave-validator.conf
+
 # Install tuned
 echo "Installing tuned..."
 apt update
